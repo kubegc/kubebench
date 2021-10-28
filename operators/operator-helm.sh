7f3aa81b41bc78cd2177b8e3520e044595066d4d
syswu <wuheng@iscas.ac.cn>
2021-10-28 13:21:36 +0800

    add README

--- operators/operator-helm.sh
+++
@@ -1,4 +0,0 @@
-
-helm install --set name=spark-app-operator --namespace kube-operator spark-app-operator .
-helm install --set name=memcached-operator --namespace kube-operator memcached-operator .
-helm install --set name=cassandra-operator --namespace kube-operator cassandra-operator .
---
+++ targets/tpcw/README.md
@@ -0,0 +1,6 @@
+
+source: https://github.com/supunab/TPC-W-Benchmark
+
+create db tpcw first
+
+curl  curl http://IP:8080/tpcw/TPCW_home_interaction
