2e900c18dcb73caf9332eb7f36089f62ad7ba65a
794774196 <794774196@qq.com>
2021-11-03 19:18:33 +0800

    bad

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 cd /root/kube-bench
+mkdir /var/lib/doslab/tpcw-test
+chmod 777 /var/lib/doslab/tpcw-test
 kubectl apply -f ./targets/tpcw/yamls/db.yaml
 kubectl apply -f ./targets/tpcw/yamls/web.yaml
-kubectl apply -f ./targets/tpcw/yamls/dbGen.yaml
